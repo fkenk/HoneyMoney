52244e633ca57bfed39cefd8162b97cc316cc354
fkenk <fkenkandfkenk@gmail.com>
2015-03-15 19:51:01 +0200

    navigation bar in fragment (MainActivity)

--- app/src/main/java/me/arthurveslo/honeymoney/MainActivity.java
+++ app/src/main/java/me/arthurveslo/honeymoney/MainActivity.java
@@ -5,6 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,8 +13,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import me.arthurveslo.honeymoney.data.FinanceDbHelper;
+import com.mikepenz.iconics.typeface.FontAwesome;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
 
+import me.arthurveslo.honeymoney.data.FinanceDbHelper;
 
 public class MainActivity extends ActionBarActivity {
     FinanceDbHelper financeDbHelper;
@@ -70,6 +77,25 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            Toolbar toolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
+            ((ActionBarActivity)getActivity()).setSupportActionBar(toolbar);
+            ((ActionBarActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            new Drawer()
+                    .withActivity(getActivity())
+                    .withToolbar(toolbar)
+                    .withActionBarDrawerToggle(true)
+                    .withHeader(R.layout.drawer_header)
+                    .addDrawerItems(
+                            new PrimaryDrawerItem().withName(R.string.drawer_item_home).withIcon(FontAwesome.Icon.faw_home).withBadge("99").withIdentifier(1),
+                            new PrimaryDrawerItem().withName(R.string.drawer_item_free_play).withIcon(FontAwesome.Icon.faw_gamepad),
+                            new PrimaryDrawerItem().withName(R.string.drawer_item_custom).withIcon(FontAwesome.Icon.faw_eye).withBadge("6").withIdentifier(2),
+                            new SectionDrawerItem().withName(R.string.drawer_item_settings),
+                            new SecondaryDrawerItem().withName(R.string.drawer_item_help).withIcon(FontAwesome.Icon.faw_cog),
+                            new SecondaryDrawerItem().withName(R.string.drawer_item_open_source).withIcon(FontAwesome.Icon.faw_question).setEnabled(false),
+                            new DividerDrawerItem(),
+                            new SecondaryDrawerItem().withName(R.string.drawer_item_contact).withIcon(FontAwesome.Icon.faw_github).withBadge("12+").withIdentifier(1)
+                    )
+                    .build();
             btnAdd = (Button) rootView.findViewById(R.id.add);
             btnAdd.setOnClickListener(this);
             btnTakeOff = (Button) rootView.findViewById(R.id.takeOff);
